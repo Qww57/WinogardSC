d6ffce35c1590fa6af833ab9195446ab6988b733
Qww57 <tresontani.quentin@gmail.com>
2016-09-29 16:12:17 +0800

    Filtering google search sentences

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -1,7 +1,9 @@
 from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
-import nltk.data
+from nltk.tokenize import sent_tokenize
 import pprint
+import os
+import nltk.data
 import urllib.request
 
 
@@ -17,58 +19,86 @@ class GoogleSearch:
     gse_results = []
 
     # Request set
-    request_set = []
+    search_terms = []
 
     # request=".*inquired.*but.*forgotten.*"
 
-    request="cat"
+    request = ""
 
-    def google_search(self, search_term, **kwargs):
+    def concatenate(self, search_terms):
+        concat = ""
+        for i in search_terms:
+            concat = concat + '.*' + i
+        concat = concat + '.*'
+        self.request = concat
+        return self.request
+
+    def google_search(self, search_terms, **kwargs):
+        self.search_terms = search_terms
+        self.request = self.concatenate(search_terms)
         service = build("customsearch", "v1", developerKey=self.api_key)
-        res = service.cse().list(q=search_term, cx=self.cse_id, **kwargs).execute()
+        res = service.cse().list(q=request, cx=self.cse_id, **kwargs).execute()
         self.gse_results = res['items']
         return self.gse_results
 
-    '''
     def display_pages(self, search_term):
         if self.gse_results is []:
             self.gse_results = self.google_search(search_term)
         for result in self.gse_results:
             pprint.pprint(result)
-    '''
 
 
-request = "cat"
+request = "raccoon"
 gs = GoogleSearch()
-results = gs.google_search(request, num=1)
+search_terms = ['asked', 'but', 'forgot']
+print(gs.concatenate(search_terms))
+results = gs.google_search(request, num=3)
 
-for result in results:
-    # Get the url link of the result page
-    url = result[u'link']
-    title = result[u'title']
-    print(url)
 
-    # Convert the HTML to string
-    f = urllib.request.urlopen(url)
-    mybytes = f.read() # read bytes from url
-    f.close()
-    mystr = mybytes.decode("utf8") # decodes bytes to string
+def tokenize(gse_results):
 
-    # Deleting all the html characters
-    soup = BeautifulSoup(mystr, "html.parser")
-    raw_text = soup.get_text()
+    results = []
 
-    # Tokenizing the text into sentences and filtering it
-    # TODO tokenize with punkt but should do it also with spaces etc
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') # Could be defined when instanciating the class
-    sentences = tokenizer.tokenize(raw_text)
-    print(len(sentences))
+    for result in gse_results:
+        # Convert the HTML to string
+        url = result[u'link']
+        print(url)
+        f = urllib.request.urlopen(url)
+        mybytes = f.read() # read bytes from url
+        f.close()
+        mystr = mybytes.decode("utf8") # decodes bytes to string
 
-    filtered = []
-    for sentence in sentences:
-        if request in sentence:
-            filtered.append(sentence)
+        # Deleting all the html characters
+        soup = BeautifulSoup(mystr, "html.parser")
+        raw_text = soup.get_text()
 
-    print(len(filtered))
-    # print(filtered[0])
-    print(filtered[15])
+        # Deleting useless empty lines
+        raw_text = os.linesep.join([s for s in raw_text.splitlines() if s])
+
+        # Tokenizing the text into sentences and filtering it
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') # Could be defined when instanciating the class
+        sentences = tokenizer.tokenize(raw_text)
+
+        set = []
+        for sentence in sentences:
+            lines = sentence.splitlines()
+            for line in lines:
+                word_groups = line.split('\\s{2,}')
+                for group in word_groups:
+                    set.append(group)
+
+        filtered = []
+        # DO for all search items
+        for sentence in set:
+            if request in sentence:
+                filtered.append(sentence)
+
+        results.extend(filtered)
+
+    return results
+
+print(len(tokenize(results)))
+
+# def filter():
+    # TODO filter sentences which are interesting
+    # Have at least a verb to express

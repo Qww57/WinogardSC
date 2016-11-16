92cf2b838348f2bf6c33f0f9a0db25f406cba2d3
Qww57 <tresontani.quentin@gmail.com>
2016-11-17 00:42:41 +0800

    Adding an example in main

--- winosolver/main.py
+++ winosolver/main.py
@@ -2,6 +2,7 @@
 
 import os
 import warnings
+from winosolver.schema.XMLParser import parse_xml
 from winosolver.schema.Schema import Schema
 from winosolver.dce.dce_bagging import DCEClassifierBagging
 from winosolver.dce.dce_solver import DirectCausalEventSolver, features
@@ -9,13 +10,52 @@ from winosolver.dce.dce_solver import DirectCausalEventSolver, features
 warnings.filterwarnings("ignore")
 
 
+def dce(current, opposite):
+    """
+    Function used to resolve a couple of schemas
+
+    :param current:
+    :param opposite:
+    :return:
+    """
+    dce_classifier = DCEClassifierBagging()
+    dce_solver = DirectCausalEventSolver()
+
+    # Process of resolving one schema
+    current.print()
+    opposite.print()
+
+    print("---- CLASSIFYING THE SCHEMA ----")
+    print(current.sentence)
+    guess = dce_classifier.classify(current)
+    current.set_type(guess)
+    print("-> Classified as " + guess)
+    print(opposite.sentence)
+    guess = dce_classifier.classify(opposite)
+    opposite.set_type(guess)
+    print("-> Classified as " + guess)
+    print(" ")
+
+    print("---- ANSWERING THE SCHEMA ----")
+    print(current.sentence)
+    answer = dce_solver.solve(current)
+    print("-> Answer: " + str(answer))
+    print(opposite.sentence)
+    answer = dce_solver.solve(opposite)
+    print("-> Answer: " + str(answer))
+    print(" ")
+
+
 def main():
     os.system("cls")
 
     # Getting the user's input
-    pre_loaded = input("Run preloaded example? (y/n)")
+    pre_loaded = input("Run preloaded examples? (y/n)")
+    print(" ")
 
     if pre_loaded is 'y' or pre_loaded is 'Y':
+
+        # First example
         sentence = "Metz football team won against the one from Paris because it was better."
         snippet = "it was better"
         pronoun = "it"
@@ -23,6 +63,24 @@ def main():
         answer_b = "the one from Paris"
         sentence_bis = "Metz football team won against the one from Paris because it was bad."
         snippet_bis = "it was bad"
+
+        current = Schema(ID=1, sentence=sentence, snip=snippet, pron=pronoun, prop1=answer_a,
+                         prop2=answer_b, answer="to be guessed", source="console")
+
+        opposite = Schema(ID=1, sentence=sentence_bis, snip=snippet_bis, pron=pronoun, prop1=answer_a,
+                          prop2=answer_b, answer="to be guessed", source="console")
+
+        print("Resolution of the first example: ")
+        dce(current, opposite)
+
+        # Schema: Pete envies Martin because/although he is very successful.
+        schemas = parse_xml()
+        current = schemas[38]
+        opposite = schemas[39]
+
+        print("Resolution of the second example: ")
+        dce(current, opposite)
+
     else:
         print("---- Enter schema to solve ----")
         sentence = input("Enter the full sentence of the schema:")
@@ -31,57 +89,27 @@ def main():
         answer_a = input("Enter answer A:")
         answer_b = input("Enter answer B:")
 
-    current = Schema(
-        ID=-1,
-        sentence=sentence,
-        snip=snippet,
-        pron=pronoun,
-        prop1=answer_a,
-        prop2=answer_b,
-        answer="to be guessed",
-        source="console"
-    )
+        current = Schema(ID=1, sentence=sentence, snip=snippet, pron=pronoun, prop1=answer_a,
+            prop2=answer_b, answer="to be guessed", source="console")
 
-    opposite = Schema(
-        ID=-1,
-        sentence=sentence_bis,
-        snip=snippet_bis,
-        pron=pronoun,
-        prop1=answer_a,
-        prop2=answer_b,
-        answer="to be guessed",
-        source="console"
-    )
+        dce_classifier = DCEClassifierBagging()
+        dce_solver = DirectCausalEventSolver()
 
-    print()
-    current.print()
+        print("---- CLASSIFYING THE SCHEMA ----")
+        print(current.sentence)
+        guess = dce_classifier.classify(current)
+        current.set_type(guess)
+        print("-> Classified as " + guess)
+        print(" ")
 
-    dce_classifier = DCEClassifierBagging()
-    dce_solver = DirectCausalEventSolver()
+        print("---- ANSWERING THE SCHEMA ----")
+        print(current.sentence)
+        answer = dce_solver.solve(current)
+        print("-> Answer: " + str(answer))
+        print(" ")
 
-    # Process of resolving one schema
-    print("---- CLASSIFYING THE SCHEMA ----")
-    guess = dce_classifier.classify(current)
-    current.set_type(guess)
-    opposite.set_type(guess)
-    print("Sentence has been classified as " + guess)
-    print(" ")
-
-    print("---- ANSWERING THE FIRST SCHEMA ----")
-    print(current.sentence)
-    answer = dce_solver.solve(current)
-    print("Answer: " + str(answer))
-    print("")
-
-    opposite.print()
-
-    print(" ")
-    print("---- ANSWERING THE SECOND SCHEMA ----")
-    print(opposite.sentence)
-    answer = dce_solver.solve(opposite)
-    print("Answer: " + str(answer))
-
-    # os.system("pause")
+    print("---- END ----")
+    os.system("pause")
 
 main()
 
